7a898562289ec11f775e73b9829f2f670e394c46
thelzycoder <mayank.prasoon@outlook.com>
2022-01-28 15:22:26 +0530

    Update to have psql as db

--- tradejournal/settings.py
+++ tradejournal/settings.py
@@ -94,8 +94,12 @@ REST_FRAMEWORK={
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tradeapp',
+        'USER': 'tradeuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
